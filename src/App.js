128247a378ab61012277708bf9d8e7b71b9f21bf
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-02-16 15:44:49 -0500

    set up basic homepage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Homepage from './pages/HomePage/HomePage.component';
 import './App.css';
+import HomePage from './pages/HomePage/HomePage.component';
 
 function App() {
   return (
     <div className="App">
-      TODO
+      <HomePage />
     </div>
   );
 }
