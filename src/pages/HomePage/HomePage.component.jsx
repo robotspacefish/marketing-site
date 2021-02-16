d4f0a71b87ff7c6e9dff29c3c0d0dba6b6c41405
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-02-16 15:48:30 -0500

    add dummy content

--- src/pages/HomePage/HomePage.component.jsx
+++ src/pages/HomePage/HomePage.component.jsx
@@ -2,7 +2,33 @@ import React from 'react';
 
 const Homepage = () => (
   <div className="homepage">
-    Homepage
+    <h1>Product Title</h1>
+
+    <section>
+      <h2>Product Info</h2>
+      <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec ullamcorper nulla non metus auctor fringilla. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis risus eget urna mollis ornare vel eu leo. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+    </section>
+
+    <section>
+      <h2>Product Features</h2>
+      <div>
+        <h3>Feature 1</h3>
+        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Sed posuere consectetur est at lobortis.</p>
+      </div>
+
+      <div>
+        <h3>Feature 2</h3>
+        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Sed posuere consectetur est at lobortis.</p>
+      </div>
+
+      <div>
+        <h3>Feature 3</h3>
+        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Sed posuere consectetur est at lobortis.</p>
+      </div>
+    </section>
+
+    <hr />
+    <button>Contact Us</button>
   </div>
 );
 
