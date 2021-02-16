5e9a50a2fc26022f311410f5ef5c89e604c44057
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-02-16 16:23:03 -0500

    FeaturePage

---
+++ src/pages/FeaturePage/FeaturePage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FeaturePage = ({ title, img, info }) => (
+  <div className="featurepage">
+    TODO FeaturePage
+    <h1>{title}</h1>
+    <img src={img} alt={title} />
+  </div>
+);
+
+export default FeaturePage;
