d6cfb0bb275f5d8553d1e51456e7d1f1622f5c59
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2021-02-16 18:43:53 -0500

    convert App to class, import data, and make features path dynamic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import HomePage from './pages/HomePage/HomePage.component';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-
 import './App.css';
 import FeaturePage from './pages/FeaturePage/FeaturePage.component';
+import PRODUCT_DATA from './product.data';
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productInfo: []
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ productInfo: PRODUCT_DATA });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path={`/features/:feature`} component={FeaturePage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
